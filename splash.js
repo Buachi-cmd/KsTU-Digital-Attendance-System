c444e2f9bb3ef93ec7c3e89f7c1de25c68fe310e
BUACHI EMPIRE <alphauponalphas@gmail.com>
2025-05-23 12:09:43 +0000

    Add files via upload

---
+++ splash.js
@@ -0,0 +1,8 @@
+setTimeout(function () {
+    window.location.href = "login.html";
+}, 6000);
+window.onload = function() {
+    setTimeout(() => {
+        document.getElementById("text").style.animation = "moveText 5s forwards";
+    }, 0 );
+};
